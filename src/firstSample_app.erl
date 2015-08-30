24781d063fbc9b06b94ba6a9773b23dfb772a4d1
Madankumar <madan.tallapalem@gmail.com>
2015-08-30 16:14:45 +0530

    initial version

---
+++ src/firstSample_app.erl
@@ -0,0 +1,26 @@
+-module(firstSample_app).
+
+-behaviour(application).
+
+%% Application callbacks
+-export([start/2, stop/1]).
+-ifdef(TEST).
+-include_lib("eunit/include/eunit.hrl").
+-endif.
+%% ===================================================================
+%% Application callbacks
+%% ===================================================================
+
+start(_StartType, _StartArgs) ->
+    firstSample_sup:start_link().
+
+stop(_State) ->
+    ok.
+
+-ifdef(TEST).
+
+simple_test() ->
+    ok = application:start(firstSample),
+    ?assertNot(undefined == whereis(firstSample_sup)).
+
+-endif.
